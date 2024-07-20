f064203a93b193c5d22c888501a16c62cd948820
Levan Asanoghli <asanoghli@outlook.com>
2024-07-20 23:41:42 +0400

    ADMIN - Sidebar fixed some bugs

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script>
+export  default {
+  setup(){
+  },
+  methods : {
+
+  }
+}
+</script>
+<template>
+<header>
+</header>
+  <main class="flex">
+    <aside class="h-dvh">
+      <AppSidebar  class="w-full"/>
+    </aside>
+    <slot/>
+  </main>
+</template>
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+export  default {
+  setup(){
+    let num = ref(false);
+
+
+    return {num};
+  },
+  methods : {
+    TestMe(){
+      this.num = !this.num;
+    }
+  }
+}
+</script>
+<template>
+  <h1 @click="TestMe">Hello WOrld - {{num}}</h1>
+</template>
