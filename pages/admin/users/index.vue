4fbecf11bcc66617f82ba48891e03871fe5c374a
Levan Asanoghli <asanoghli@outlook.com>
2024-07-26 00:32:15 +0400

    ADMIN - USERS - fixed bug for SELECT

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -27,6 +27,9 @@ export default {
   computed : {
     GetIsEveryUserChecked(){
       return this.users.every(user=>user.checked);
+    },
+    GetIsAnyoneChecked(){
+      return this.users.some(user=>user.checked);
     }
   }
 }
@@ -37,9 +40,14 @@ export default {
       <thead class="bg-gray-200">
       <tr>
         <th scope="col" class="px-6 size-0 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-          <svg v-show="!GetIsEveryUserChecked" @click="CheckAllUsers($event,true)" xmlns="http://www.w3.org/2000/svg" class="size-8"  viewBox="0 0 32 32"><path fill="currentColor" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/></svg>
-          <svg v-show="GetIsEveryUserChecked"  @click="CheckAllUsers($event,false)" xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/><path fill="currentColor" d="m14 21.5l-5-4.96L10.59 15L14 18.35L21.41 11L23 12.58z"/></svg>
-
+          <span v-show="GetIsAnyoneChecked">
+<!--If All Of Them Checked-->
+          <svg v-show="GetIsEveryUserChecked"  @click="CheckAllUsers($event,false)" xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/><path fill="gray" d="m14 21.5l-5-4.96L10.59 15L14 18.35L21.41 11L23 12.58z"/></svg>
+<!--         If SomeOne Checked-->
+          <svg v-show="GetIsAnyoneChecked && !GetIsEveryUserChecked"  @click="CheckAllUsers($event,true)"  xmlns="http://www.w3.org/2000/svg" class="size-8"   viewBox="0 0 32 32"><path fill="gray" d="M10 14h12v4H10z"/><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/></svg>
+          </span>
+<!-- If No One Checked-->
+          <svg v-show="!GetIsEveryUserChecked && !GetIsAnyoneChecked" @click="CheckAllUsers($event,true)" xmlns="http://www.w3.org/2000/svg" class="size-8"  viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/></svg>
         </th>
         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
           FULLNAME
@@ -65,7 +73,7 @@ export default {
       <tr v-for="user in users" :key="user.id">
         <td class="px-6 whitespace-nowrap">
           <label @click="user.checked = !user.checked">
-            <svg v-show="user.checked" xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/><path fill="currentColor" d="m14 21.5l-5-4.96L10.59 15L14 18.35L21.41 11L23 12.58z"/></svg>
+            <svg v-show="user.checked" xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/><path fill="gray" d="m14 21.5l-5-4.96L10.59 15L14 18.35L21.41 11L23 12.58z"/></svg>
             <svg v-show="!user.checked" xmlns="http://www.w3.org/2000/svg" class="size-8"  viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/></svg>
           </label>
         </td>
