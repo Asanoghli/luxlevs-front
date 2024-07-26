dd01de8b1bd83aa6c6e3753c9703e533d890d4fa
Levan Asanoghli <asanoghli@outlook.com>
2024-07-26 04:46:29 +0400

    ADMIN - USERS, checked color added for CHECKED USERS

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -142,7 +142,7 @@ export default {
       </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-200">
-      <tr class="h-14" v-for="user in users" :key="user.id">
+      <tr class="h-14" v-for="user in users" :key="user.id" :class="user.checked ? 'bg-gray-300' : null">
         <td class="px-6 whitespace-nowrap">
           <label @click="user.checked = !user.checked">
             <svg v-show="user.checked" xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 32 32"><path fill="gray" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M6 26V6h20v20Z"/><path fill="gray" d="m14 21.5l-5-4.96L10.59 15L14 18.35L21.41 11L23 12.58z"/></svg>
