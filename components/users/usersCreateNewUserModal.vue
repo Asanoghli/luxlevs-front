67d33e5cdb9d3e972473500466572ac577e93c16
Levan Asanoghli <asanoghli@outlook.com>
2024-07-27 17:04:05 +0400

    ADMIN - USERS, added 3XL and 4XL to TAILWIND and fixed some bugs

--- components/users/usersCreateNewUserModal.vue
+++ components/users/usersCreateNewUserModal.vue
@@ -34,7 +34,7 @@ export  default {
             From: "opacity-100 translate-y-0 sm:scale-100"
             To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         -->
-        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:w-3/4  xl:w-1/3 md:w-2/3 lg:w-1/2">
+        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:w-3/4  xl:w-1/3 2xl:w-1/3 3xl:w-1/3 4xl:w-1/4  md:w-2/3 lg:w-1/2">
           <div class="px-6 bg-gray-800 w-full py-6 flex justify-between items-center">
             <h3 class="text-xl text-gray-400">მომხმარებლის დამატება</h3>
            <button @click="usersStore.ToggleCreateUserModal">
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '1770px',
+                '4xl': '2000px',
+            },
+        },
+    },
+    plugins: [],
+}
