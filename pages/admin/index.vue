2a8293a90527c2443fc417c3066af07e4ab81271
Levan Asanoghli <asanoghli@outlook.com>
2024-07-29 00:52:11 +0400

    ADMIN - LOGIN, changed some styles

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -76,47 +76,49 @@ export default {
           <span class="header-slogan-text">{{ $t('admin.login.header-slogan') }}</span>
         </div>
       </div>
-      <hr class="border-spacing-1 border-gray-700 w-full">
-      <form action="" class="flex justify-start w-full flex-col gap-4">
-        <div class="flex flex-col  w-full gap-2 ">
-          <input v-model.lazy="v.username.$model" class="focus:outline-0 p-3 rounded bg-gray-900 text-gray-300"
-                 :class="[GetInvalidClassesIfDirtyAndInvalidUsername, GetValidClassesIfDirtyAndValidUsername]"
-                 type="text" :placeholder="$t('admin.login.input-username-placeholder')">
-          <span v-show="v.username.$error" class="text-red-500">{{ $t('admin.login.username-is-required') }}</span>
+      <div class="w-full p-4 pb-6 flex flex-col gap-8 ">
+        <form action="" class="flex justify-start w-full flex-col gap-4">
+          <div class="flex flex-col  w-full gap-2 ">
+            <input v-model.lazy="v.username.$model" class="focus:outline-0 p-3 rounded bg-gray-900 text-gray-300"
+                   :class="[GetInvalidClassesIfDirtyAndInvalidUsername, GetValidClassesIfDirtyAndValidUsername]"
+                   type="text" :placeholder="$t('admin.login.input-username-placeholder')">
+            <span v-show="v.username.$error" class="text-red-500">{{ $t('admin.login.username-is-required') }}</span>
+          </div>
+          <div class="flex flex-col  w-full gap-3">
+            <input v-model.lazy="v.password.$model"
+                   class="focus:outline-0 p-3 rounded bg-gray-900 text-gray-300" type="text"
+                   :class="[GetInvalidClassesIfDirtyAndInvalidPassword, GetValidClassesIfDirtyAndValidPassword]"
+                   :placeholder="$t('admin.login.input-password-placeholder')">
+            <span v-show="v.password.$error" class="text-red-500">{{$t('admin.login.password-is-required')}}</span>
+            <span v-show="isPasswordInvalid" class="text-red-500">{{ $t('admin.login.incorrect-username-or-password') }}</span>
+          </div>
+          <button  @click.prevent="SignIn" class="btn-login ">
+            <span class="btn-login-text">{{ $t('admin.login.btn-login') }}</span>
+          </button>
+        </form>
+        <div class="text-gray-300 flex justify-center gap-4 w-full">
+          <button @click="ChangeLanguage('ge')">
+            <img src="/ge.png" class="size-10" alt="">
+          </button>
+          <button>
+            <img @click="ChangeLanguage('en')" src="/uk.png" class="size-10" alt="">
+          </button>
         </div>
-        <div class="flex flex-col  w-full gap-3">
-          <input v-model.lazy="v.password.$model"
-                 class="focus:outline-0 p-3 rounded bg-gray-900 text-gray-300" type="text"
-                 :class="[GetInvalidClassesIfDirtyAndInvalidPassword, GetValidClassesIfDirtyAndValidPassword]"
-                 :placeholder="$t('admin.login.input-password-placeholder')">
-          <span v-show="v.password.$error" class="text-red-500">{{$t('admin.login.password-is-required')}}</span>
-          <span v-show="isPasswordInvalid" class="text-red-500">{{ $t('admin.login.incorrect-username-or-password') }}</span>
-        </div>
-        <button  @click.prevent="SignIn" class="btn-login ">
-          <span class="btn-login-text">{{ $t('admin.login.btn-login') }}</span>
-        </button>
-      </form>
-      <div class="text-gray-300 flex justify-start gap-4 w-full">
-        <button @click="ChangeLanguage('ge')">
-          <img src="/ge.png" class="size-10" alt="">
-        </button>
-        <button>
-          <img @click="ChangeLanguage('en')" src="/uk.png" class="size-10" alt="">
-        </button>
       </div>
+
     </div>
   </div>
 </template>
 <style scoped>
 .modal-container{
-@apply h-dvh bg-gradient-to-br from-gray-600 to-gray-950 flex justify-center items-center flex-col;
+@apply h-dvh bg-gradient-to-br from-gray-700 to-gray-950 flex justify-center items-center flex-col;
 }
 .modal-wrapper{
-  @apply flex gap-5 justify-center drop-shadow-lg items-center flex-col lg:h-fit bg-gray-950 lg:w-1/4 rounded-md p-4 py-7;
+  @apply flex gap-5 justify-center shadow-blue-600 shadow-2xl items-center flex-col lg:h-fit bg-gray-950 lg:w-1/4 rounded-md overflow-hidden ;
 }
 
 .logo-and-header-container{
-  @apply bg-gradient-to-br from-blue-500 to-blue-950 w-full flex  items-center justify-evenly rounded;
+  @apply bg-gradient-to-br from-blue-500 to-blue-950 w-full flex  items-center justify-evenly  ;
 }
 
 .header-and-slogan-container{
@@ -131,7 +133,7 @@ export default {
 }
 
 .btn-login{
-  @apply w-full bg-blue-600 rounded p-3 hover:bg-blue-500  flex justify-center items-center gap-3 mt-4;
+  @apply w-full bg-blue-600 rounded p-3 hover:bg-blue-500  flex justify-center items-center gap-3 mt-6;
 }
 .btn-login-text{
   @apply text-white text-xl;
