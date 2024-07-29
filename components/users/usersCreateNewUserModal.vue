e9dfa90ecee909b76363c7bfb8aa391455e27d1c
Levan Asanoghli <asanoghli@outlook.com>
2024-07-30 02:42:57 +0400

    ADMIN - USERS, Create User Validations ADDED

--- components/users/usersCreateNewUserModal.vue
+++ components/users/usersCreateNewUserModal.vue
@@ -1,17 +1,18 @@
 <script>
 import useVuelidate from "@vuelidate/core";
-import {required, minLength, maxLength, createI18nMessage, email,requiredIf} from '@vuelidate/validators'
+import {required, minLength, maxLength, createI18nMessage, email, requiredIf} from '@vuelidate/validators'
 import {ValidationConstants} from "~/constants/ValidationConstants.js";
 import {
   PasswordMustContainOneCharacter,
   PasswordMustContainsOneLowerCase, PasswordMustContainsOneNumeric,
   PasswordMustContainsOneUpperCase
 } from "~/validators/customvalidations.js";
+
 export default {
   setup() {
     let {$i18n} = useNuxtApp();
 
-    const withI18nMessage = createI18nMessage({t:$i18n.t});
+    const withI18nMessage = createI18nMessage({t: $i18n.t});
 
     let usersStore = useUsersStore();
     let userModel = reactive({
@@ -22,36 +23,36 @@ export default {
       password: ''
     });
     let rules = {
-      firstName : {
-          required : withI18nMessage(required,{messagePath:()=>'admin.users.create.firstname-required'}),
-          minLength : withI18nMessage(minLength(ValidationConstants.ADMIN.FIRSTNAME_MIN_LENGTH),{messagePath:_=>'admin.users.create.firstname-minlength'}),
-          maxLength : withI18nMessage(maxLength(ValidationConstants.ADMIN.FIRSTNAME_MAX_LENGTH),{messagePath:_=>'admin.users.create.firstname-maxlength'}),
+      firstName: {
+        required: withI18nMessage(required, {messagePath: () => 'admin.users.create.firstname-required'}),
+        minLength: withI18nMessage(minLength(ValidationConstants.ADMIN.FIRSTNAME_MIN_LENGTH), {messagePath: _ => 'admin.users.create.firstname-minlength'}),
+        maxLength: withI18nMessage(maxLength(ValidationConstants.ADMIN.FIRSTNAME_MAX_LENGTH), {messagePath: _ => 'admin.users.create.firstname-maxlength'}),
       },
-      lastName : {
-        required : withI18nMessage(required,{messagePath:()=>'admin.users.create.lastname-required',withArguments:true}),
-        minLength : withI18nMessage(minLength(ValidationConstants.ADMIN.LASTNAME_MIN_LENGTH),{messagePath:()=>'admin.users.create.lastname-minlength'}),
-        maxLength : withI18nMessage(maxLength(ValidationConstants.ADMIN.LASTNAME_MAX_LENGTH),{messagePath:()=>'admin.users.create.lastname-maxlength'}),
+      lastName: {
+        required: withI18nMessage(required, {messagePath: () => 'admin.users.create.lastname-required'}),
+        minLength: withI18nMessage(minLength(ValidationConstants.ADMIN.LASTNAME_MIN_LENGTH), {messagePath: () => 'admin.users.create.lastname-minlength'}),
+        maxLength: withI18nMessage(maxLength(ValidationConstants.ADMIN.LASTNAME_MAX_LENGTH), {messagePath: () => 'admin.users.create.lastname-maxlength'}),
       },
-      email : {
-        required : withI18nMessage(required,{messagePath:_=>'admin.users.create.email-required'}),
-        invalidEmail: withI18nMessage(email,{messagePath:_=>'admin.users.create.email-invalid'}),
-        minLength : withI18nMessage(minLength(ValidationConstants.ADMIN.EMAIL_MIN_LENGTH),{messagePath:()=>'admin.users.create.email-minlength'}),
-        maxLength : withI18nMessage(maxLength(ValidationConstants.ADMIN.EMAIL_MAX_LENGTH),{messagePath:()=>'admin.users.create.email-maxlength'})
+      email: {
+        required: withI18nMessage(required, {messagePath: _ => 'admin.users.create.email-required'}),
+        invalidEmail: withI18nMessage(email, {messagePath: _ => 'admin.users.create.email-invalid'}),
+        minLength: withI18nMessage(minLength(ValidationConstants.ADMIN.EMAIL_MIN_LENGTH), {messagePath: () => 'admin.users.create.email-minlength'}),
+        maxLength: withI18nMessage(maxLength(ValidationConstants.ADMIN.EMAIL_MAX_LENGTH), {messagePath: () => 'admin.users.create.email-maxlength'})
       },
-      password : {
-        required : withI18nMessage(required,{messagePath:_=>'admin.users.create.password-required'}),
-        minLength : withI18nMessage(minLength(ValidationConstants.ADMIN.PASSWORD_MIN_LENGTH),{messagePath:()=>'admin.users.create.password-minlength'}),
-        maxLength : withI18nMessage(maxLength(ValidationConstants.ADMIN.PASSWORD_MAX_LENGTH),{messagePath:()=>'admin.users.create.password-maxlength'}),
-        mustContainsOneCharacter : withI18nMessage(PasswordMustContainOneCharacter,{messagePath:()=>'admin.users.create.password-not-contains-character'}),
-        mustContainsOneNumeric : withI18nMessage(PasswordMustContainsOneNumeric,{messagePath:()=>'admin.users.create.password-not-contains-numeric'}),
-        mustContainsOneLowerCase : withI18nMessage(PasswordMustContainsOneLowerCase,{messagePath:()=>'admin.users.create.password-not-contains-lowercase'}),
-        mustContainsOneUpperCase : withI18nMessage(PasswordMustContainsOneUpperCase,{messagePath:()=>'admin.users.create.password-not-contains-uppercase'}),
+      password: {
+        required: withI18nMessage(required, {messagePath: _ => 'admin.users.create.password-required'}),
+        minLength: withI18nMessage(minLength(ValidationConstants.ADMIN.PASSWORD_MIN_LENGTH), {messagePath: () => 'admin.users.create.password-minlength'}),
+        maxLength: withI18nMessage(maxLength(ValidationConstants.ADMIN.PASSWORD_MAX_LENGTH), {messagePath: () => 'admin.users.create.password-maxlength'}),
+        mustContainsOneCharacter: withI18nMessage(PasswordMustContainOneCharacter, {messagePath: () => 'admin.users.create.password-min-one-symbol'}),
+        mustContainsOneNumeric: withI18nMessage(PasswordMustContainsOneNumeric, {messagePath: () => 'admin.users.create.password-min-one-digit'}),
+        mustContainsOneLowerCase: withI18nMessage(PasswordMustContainsOneLowerCase, {messagePath: () => 'admin.users.create.password-min-one-lower'}),
+        mustContainsOneUpperCase: withI18nMessage(PasswordMustContainsOneUpperCase, {messagePath: () => 'admin.users.create.password-min-one-upper'}),
       }
 
     }
-    let v = useVuelidate(rules,userModel);
+    let v = useVuelidate(rules, userModel);
 
-    return {usersStore,v};
+    return {usersStore, v};
   },
   methods: {
     async CreateNewUser() {
@@ -68,6 +69,9 @@ export default {
       })
       console.log(res);
     }
+  },
+  computed : {
+    
   }
 }
 </script>
@@ -99,7 +103,7 @@ export default {
         <div
             class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:w-3/4  xl:w-1/3 2xl:w-1/3 3xl:w-1/3 4xl:w-1/4  md:w-2/3 lg:w-1/2">
           <div class="px-6 bg-gray-800 w-full py-6 flex justify-between items-center">
-            <h3 class="text-xl text-gray-400">მომხმარებლის დამატება</h3>
+            <h3 class="text-xl text-gray-400 uppercase">{{$t('admin.users.add-user')}}</h3>
             <button @click="usersStore.ToggleCreateUserModal">
               <svg xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 48 48">
                 <g fill="none" stroke="#9CA3AF" stroke-linejoin="round" stroke-width="4">
@@ -112,33 +116,31 @@ export default {
           <div class="bg-gray-900 px-4 pb-4 pt-5 sm:p-6 sm:pb-4 w-full">
             <div v-show="v.$errors.length > 0" class="h-auto bg-gray-800 mb-5 p-5 rounded">
               <ul class="list-disc w-full marker:m-0 marker:p-0 list-inside m-0 p-0">
-                <li class="text-red-500 p-1 " v-for="error in v.$errors">{{error.$message}}</li>
+                <li class="text-red-500 p-1 " v-for="error in v.$errors">{{ error.$message }}</li>
               </ul>
             </div>
 
             <div class="flex items-start flex-col gap-3">
               <div class="flex items-center justify-start w-full">
-                <label class="w-1/3 text-gray-400" for="">სახელი</label>
-                  <input v-model.lazy="v.firstName.$model" class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
-              </div>
+                <label class="w-1/3 text-gray-400" for="">{{ $t('admin.users.firstname') }}</label>
+                <input v-model.lazy="v.firstName.$model"
+                       class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
+              </div> <!--firstname -->
               <div class="flex items-center justify-start w-full">
-                <label class="w-1/3 text-gray-400" for="">გვარი</label>
-                <input v-model.lazy="v.lastName.$model" class="p-2 bg-gray-600 text-gray-300 rounded w-auto grow  focus:outline-0" type="text">
-              </div>
+                <label class="w-1/3 text-gray-400" for="">{{ $t('admin.users.lastname') }}</label>
+                <input v-model.lazy="v.lastName.$model"
+                       class="p-2 bg-gray-600 text-gray-300 rounded w-auto grow  focus:outline-0" type="text">
+              </div> <!--lastname -->
               <div class="flex items-center justify-start w-full">
-                <label class="w-1/3 text-gray-400" for="">ელ.ფოსტა</label>
-                <input class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
-              </div>
+                <label class="w-1/3 text-gray-400" for="">{{ $t('admin.users.email') }}</label>
+                <input v-model.lazy="v.email.$model"
+                       class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
+              </div> <!--email -->
               <div class="flex items-center justify-start w-full">
-                <label class="w-1/3 text-gray-400" for="">მომხ. სახელი</label>
-                <input class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
-              </div>
-              <div class="flex items-center justify-start w-full">
-                <label class="w-1/3 text-gray-400" for="">პაროლი</label>
-                <input class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
-              </div>
+                <label class="w-1/3 text-gray-400" for="">{{ $t('admin.users.password') }}</label>
+                <input v-model.k.lazy="v.password.$model" class="p-2 bg-gray-600 text-gray-300   rounded w-auto grow  focus:outline-0" type="text">
+              </div> <!--password -->
             </div>
-<!--            <p class="text-white">{{JSON.stringify(v.firstName)}}</p>-->
 
           </div>
           <div class="bg-gray-800 px-4 py-3 flex flex-row gap-3">
--- components/users/usersTableSearchBar.vue
+++ components/users/usersTableSearchBar.vue
@@ -10,23 +10,23 @@ export  default {
 <template>
   <div class="bg-gray-700 pt-2 px-3  flex items-center justify-between flex-col w-full gap-1 lg:flex-row lg:flex-nowrap lg:py-2 md:flex-row md:flex-wrap xl:justify-between  xl:px-3 md:py-2 py-2 xl:w-auto">
     <div class="flex  gap-2 justify-center items-center w-full lg:order-2 lg:w-full lg:flex-shrink xl:w-1/2 xl:flex-grow xl:px-4 lg:px-4">
-      <input class=" focus:outline-0 p-3 sm:full box-content text-white bg-gray-900 w-full lg:w-full  rounded" type="text" placeholder="Search">
+      <input class=" focus:outline-0 p-3 sm:full box-content text-white bg-gray-900 w-full lg:w-full  rounded" type="text" :placeholder="$t('admin.users.search')">
       <button class="p-3 px-5 h-full flex flex-row items-center justify-center gap-2 bg-blue-600 rounded hover:bg-blue-700">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-6" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="white" d="M10.5 4a6.5 6.5 0 1 0 0 13a6.5 6.5 0 0 0 0-13M2 10.5a8.5 8.5 0 1 1 15.176 5.262l3.652 3.652a1 1 0 0 1-1.414 1.414l-3.652-3.652A8.5 8.5 0 0 1 2 10.5M9.5 7a1 1 0 0 1 1-1a4.5 4.5 0 0 1 4.5 4.5a1 1 0 1 1-2 0A2.5 2.5 0 0 0 10.5 8a1 1 0 0 1-1-1"/></g></svg>
-        <span class="text-white hidden sm:inline-block lg:hidden xl:block">Search</span>
+        <span class="text-white hidden sm:inline-block lg:hidden xl:block">{{ $t('admin.users.search') }}</span>
       </button>
     </div>
     <div class="flex justify-end w-full lg:order-3 md:order-2  md:w-auto md:grow lg:w-auto xl:auto xl:max-w-max xl:flex-grow-0 ">
-      <button @click="usersStore.ToggleCreateUserModal" class="flex text-white p-3 bg-blue-600  items-center md:px-5  font-thin rounded  hover:bg-blue-700  w-full justify-center gap-3 lg:w-full  xl:w-full">
+      <button @click="usersStore.ToggleCreateUserModal" class="flex text-white p-3 bg-blue-600  items-center md:px-5  rounded  hover:bg-blue-700  w-full justify-center gap-3 lg:w-full  xl:w-full">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-6" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M16 14a5 5 0 0 1 5 5v2a1 1 0 1 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 1 1-2 0v-2a5 5 0 0 1 5-5zm4-6a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 1 1 0-2h1V9a1 1 0 0 1 1-1m-8-6a5 5 0 1 1 0 10a5 5 0 0 1 0-10m0 2a3 3 0 1 0 0 6a3 3 0 0 0 0-6"/></g></svg>
-        <span class="lg:hidden xl:block ">მომხმარებლის დამატება</span>
+        <span class="lg:hidden xl:block uppercase">{{$t('admin.users.add-user')}}</span>
       </button>
     </div>
     <div class="w-full lg:order-1 md:order-1  sm:w-full lg:w-fit  md:w-1/3 lg:flex-shrink xl:flex-grow-0  xl:auto">
       <button  class="p-3 px-7 bg-red-600 text-white  rounded disabled:bg-red-900 disabled:cursor-not-allowed enabled:hover:bg-red-700 w-full xl:w-full md:w-full flex items-center justify-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 min-w-5"  viewBox="0 0 24 24"><path fill="white" fill-rule="evenodd" d="m6.774 6.4l.812 13.648a.8.8 0 0 0 .798.752h7.232a.8.8 0 0 0 .798-.752L17.226 6.4h1.203l-.817 13.719A2 2 0 0 1 15.616 22H8.384a2 2 0 0 1-1.996-1.881L5.571 6.4zM9.5 9h1.2l.5 9H10zm3.8 0h1.2l-.5 9h-1.2zM4.459 2.353l15.757 2.778a.5.5 0 0 1 .406.58L20.5 6.4L3.758 3.448l.122-.69a.5.5 0 0 1 .579-.405m6.29-1.125l3.94.695a.5.5 0 0 1 .406.58l-.122.689l-4.924-.869l.122-.689a.5.5 0 0 1 .579-.406z"/></svg>
         <span class="flex justify-center items-center gap-1">
-          <span class="sm:block md:block lg:hidden xl:block">DELETE</span>
+          <span class="sm:block md:block lg:hidden xl:block">{{ $t('admin.users.delete') }}</span>
           <span>(5)</span>
         </span>
 
