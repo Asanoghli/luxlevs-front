bdec9b27338611ccd2c75fcdbcc5b1277831048a
Levan Asanoghli <asanoghli@outlook.com>
2024-08-05 00:25:14 +0400

    ADMIN - USERS, added NuxtUI Pagination

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@pinia/nuxt'],
+    modules: [ '@nuxtjs/i18n', '@pinia/nuxt', "@nuxt/ui"],
     runtimeConfig: {
         web_api_admin_url: 'http://localhost:8080/admin/'
     },
